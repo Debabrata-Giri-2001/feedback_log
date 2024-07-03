64120276a1ecbe5c0c054d81460fd92abb797880
Debabrata Giri <debabratagiri@email.com>
2024-07-03 10:29:42 +0530

    code verify

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,59 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, code } = await request.json();
+
+    const decodeUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({ userName: decodeUsername });
+
+    if (!user) {
+      return Response.json(
+        { success: false, message: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    // Check if the code is correct and not expired
+    const isCodeValid = user?.varifyCode === code;
+    const isCodeNotExpired = new Date(user.varifyCodeExpiry) > new Date();
+
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+
+      return Response.json(
+        { success: true, message: "Account verified successfully" },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Verification code has expired. Please sign up again to get a new code",
+        },
+        { status: 400 }
+      );
+    }else{
+        return Response.json(
+            { success: false, message: 'Incorrect verification code' },
+            { status: 200 }
+          );
+    }
+
+
+  } catch (error) {
+    console.error("Error in checking verify user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking verify user",
+      },
+      { status: 500 }
+    );
+  }
+}
