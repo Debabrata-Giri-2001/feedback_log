83dc0c5677528ca2d1bac60fd81a278a9c30ddd5
Debabrata Giri <debabratagiri@email.com>
2024-07-07 22:31:51 +0530

    added public icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'Feedback Log',
   description: 'create your own review',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
